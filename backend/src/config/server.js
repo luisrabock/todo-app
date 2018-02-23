caefbecb5296452d88465253586b53997dc0ccc8
Luis Rabock <luis.rabock@hotmail.com>
2018-02-23 00:42:45 -0300

    Alterações diversas da API

--- backend/src/config/server.js
+++ backend/src/config/server.js
@@ -4,9 +4,12 @@ const bodyParser = require('body-parser')
 const express = require('express')
 const server = express()
 
-server.use(bodyParser.urlencoded({extended: true}))
+server.use(bodyParser.urlencoded({ extended: true }))
 server.use(bodyParser.json())
 
-server.listen(port, function(){
-    console.log(`Servidor escutando na porta: ${port}`)
+
+server.listen(port, function() {
+    console.log(`BACKEND is running on port ${port}.`)
 })
+
+module.exports = server
