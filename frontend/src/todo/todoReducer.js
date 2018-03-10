ece9854bd92cdef250196e0285be15578b6cb253
Luis Rabock <luis.rabock@hotmail.com>
2018-03-10 00:36:57 -0300

    Search results for formList with redux

--- frontend/src/todo/todoReducer.js
+++ frontend/src/todo/todoReducer.js
@@ -1,24 +1,11 @@
-const INITIAL_STATE = {
-    description: 'Ler livro',
-    list: [{
-        _id:1,
-        description: 'Pagar fatura do cartão',
-        done: true
-    },{
-        _id:2,
-        description:'Lavar a louça',
-        done:false
-    }, {
-        _id:3,
-        description:'Estudar mais',
-        done:false
-    }]
-}
+const INITIAL_STATE = {description: 'Ler livro', list: [] }
 
 export default (state = INITIAL_STATE, action) => {
     switch(action.type) {
         case 'DESCRIPTION_CHANGE':
             return {...state, description: action.payload}
+        case 'TODO_SEARCHED':
+            return {...state, list: action.payload.data}
         default:
             return state
     }
